01212045502012a183c9d1c1add9cdd9677de622
Anmol Jhamb <talktoanmol@outlook.com>
2023-04-22 21:36:52 +0530

    form elements added, along with all the floating labels and what not

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -1,17 +1,71 @@
-import React from "react";
-import { Button, Container, Form, Row } from "react-bootstrap";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { Button, Container, FloatingLabel, Form, Row } from "react-bootstrap";
 
 const SignUp = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [cpassword, setCpassword] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const handleOnSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        setLoading(true);
+    };
+
     return (
         <>
             <Container id="signUp" className="p-3">
                 <Row>
-                    <h3>Get Started and Sign Up</h3>
+                    <h2>Get Started and Sign Up</h2>
                 </Row>
-                <Form>
-                    <Form.Group></Form.Group>
+                <Form className="m-2" onSubmit={handleOnSubmit}>
+                    <FloatingLabel className="m-2" label={"Email"}>
+                        <Form.Control
+                            type="email"
+                            placeholder="example@abc.com"
+                            value={email}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                setEmail(e.target.value);
+                            }}
+                            required
+                        />
+                    </FloatingLabel>
+                    <FloatingLabel className="m-2" label={"Password"}>
+                        <Form.Control
+                            type="password"
+                            placeholder="Your Password"
+                            value={password}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                setPassword(e.target.value);
+                            }}
+                            required
+                        />
+                    </FloatingLabel>
+                    <FloatingLabel className="m-2" label={"Confirm Password"}>
+                        <Form.Control
+                            type="password"
+                            placeholder="Confirm Your Password"
+                            value={cpassword}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                setCpassword(e.target.value);
+                            }}
+                            required
+                        />
+                    </FloatingLabel>
+                    <Button
+                        type="submit"
+                        className="w-100 m-2"
+                        disabled={loading}
+                    >
+                        Sign Up
+                    </Button>
                 </Form>
-                <Button>Sign Up</Button>
+                <Row>
+                    <h6>
+                        If you have an account already, then{" "}
+                        <a href="/login">Log In</a>
+                    </h6>
+                </Row>
             </Container>
         </>
     );
