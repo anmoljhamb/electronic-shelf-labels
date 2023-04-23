71b207582e573aad9bc5346bfea8916794e4b7e9
Anmol Jhamb <talktoanmol@outlook.com>
2023-04-23 19:34:38 +0530

    getvalue function added

--- server/dist/index.js
+++ server/dist/index.js
@@ -8,9 +8,11 @@ const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+const FILENAME = "data.json";
 const loadJSON = (fileName) => {
     return JSON.parse(fs_1.default.readFileSync(path_1.default.join(__dirname, "..", fileName), "utf-8"));
 };
+let data = loadJSON(FILENAME);
 const saveJSON = (fileName, data) => {
     fs_1.default.writeFileSync(path_1.default.join(__dirname, "..", fileName), JSON.stringify(data));
 };
@@ -24,16 +26,38 @@ app.get("/register", (req, res) => {
 app.get("/setPrice", (req, res) => {
     let { productId, price } = req.query;
     productId = productId;
-    let data = loadJSON("data.json");
-    data = Object.assign({}, data);
-    if (productId in data) {
-        data[productId].push({ price, time: new Date().toISOString() });
+    let _data = loadJSON(FILENAME);
+    _data = Object.assign({}, _data);
+    if (productId in _data) {
+        _data[productId].push({ price, time: new Date().toISOString() });
     }
     else {
-        data[productId] = [{ price, time: new Date().toISOString() }];
+        _data[productId] = [{ price, time: new Date().toISOString() }];
     }
-    saveJSON("data.json", data);
+    saveJSON(FILENAME, _data);
+    data = _data;
     return res.status(200).json({ message: "Ok" });
+});
+app.get("/getPrice", (req, res) => {
+    let { productId } = req.query;
+    productId = productId;
+    console.log(data);
+    if (productId in data) {
+        if (data[productId].length >= 2) {
+            let lastValue = data[productId].at(-1).price;
+            let secondLast = data[productId].at(-2).price;
+            lastValue = Number.parseFloat(lastValue);
+            secondLast = Number.parseFloat(secondLast);
+            if (lastValue > secondLast) {
+                return res.send(`${lastValue}`);
+            }
+            return res.send(`${lastValue} ${secondLast}`);
+        }
+        else {
+            return res.send(data[productId].at(-1));
+        }
+    }
+    return res.send("Not Found");
 });
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,15 @@ import fs from "fs";
 
 const app = express();
 const port = process.env.PORT || 8080;
+const FILENAME = "data.json";
 
 const loadJSON = (fileName: string) => {
     return JSON.parse(
         fs.readFileSync(path.join(__dirname, "..", fileName), "utf-8")
     );
 };
+
+let data: any = loadJSON(FILENAME);
 
 const saveJSON = (fileName: string, data: any) => {
     fs.writeFileSync(
@@ -30,18 +33,47 @@ app.get("/register", (req, res) => {
 app.get("/setPrice", (req, res) => {
     let { productId, price } = req.query;
     productId = productId as string;
-    let data = loadJSON("data.json");
-    data = { ...data };
 
-    if (productId in data) {
-        data[productId].push({ price, time: new Date().toISOString() });
+    let _data = loadJSON(FILENAME);
+    _data = { ..._data };
+
+    if (productId in _data) {
+        _data[productId].push({ price, time: new Date().toISOString() });
     } else {
-        data[productId] = [{ price, time: new Date().toISOString() }];
+        _data[productId] = [{ price, time: new Date().toISOString() }];
     }
 
-    saveJSON("data.json", data);
+    saveJSON(FILENAME, _data);
+    data = _data;
 
     return res.status(200).json({ message: "Ok" });
+});
+
+app.get("/getPrice", (req, res) => {
+    let { productId } = req.query;
+    productId = productId as string;
+
+    console.log(data);
+
+    if (productId in data) {
+        if (data[productId].length >= 2) {
+            let lastValue = data[productId].at(-1).price;
+            let secondLast = data[productId].at(-2).price;
+
+            lastValue = Number.parseFloat(lastValue);
+            secondLast = Number.parseFloat(secondLast);
+
+            if (lastValue > secondLast) {
+                return res.send(`${lastValue}`);
+            }
+
+            return res.send(`${lastValue} ${secondLast}`);
+        } else {
+            return res.send(data[productId].at(-1));
+        }
+    }
+
+    return res.send("Not Found");
 });
 
 app.listen(port, () => {
