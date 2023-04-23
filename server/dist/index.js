3ad9f226dcca4bb85cf7cd5de72111d178ada5e8
Anmol Jhamb <talktoanmol@outlook.com>
2023-04-23 19:21:54 +0530

    save data to data.json

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,14 +4,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+const loadJSON = (fileName) => {
+    return JSON.parse(fs_1.default.readFileSync(path_1.default.join(__dirname, "..", fileName), "utf-8"));
+};
+const saveJSON = (fileName, data) => {
+    fs_1.default.writeFileSync(path_1.default.join(__dirname, "..", fileName), JSON.stringify(data));
+};
 app.get("/", (req, res) => {
     return res.send("Express Typescript on Vercel");
 });
 app.get("/register", (req, res) => {
     console.log(req.query);
     res.status(200).json({ message: "Ok" });
+});
+app.get("/setPrice", (req, res) => {
+    let { productId, price } = req.query;
+    productId = productId;
+    let data = loadJSON("data.json");
+    data = Object.assign({}, data);
+    if (productId in data) {
+        data[productId].push(price);
+    }
+    else {
+        data[productId] = [price];
+    }
+    saveJSON("data.json", data);
+    return res.status(200).json({ message: "Working." });
 });
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,22 @@
 import express, { Request, Response } from "express";
+import path from "path";
+import fs from "fs";
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+const loadJSON = (fileName: string) => {
+    return JSON.parse(
+        fs.readFileSync(path.join(__dirname, "..", fileName), "utf-8")
+    );
+};
+
+const saveJSON = (fileName: string, data: any) => {
+    fs.writeFileSync(
+        path.join(__dirname, "..", fileName),
+        JSON.stringify(data)
+    );
+};
 
 app.get("/", (req: Request, res: Response) => {
     return res.send("Express Typescript on Vercel");
@@ -10,6 +25,23 @@ app.get("/", (req: Request, res: Response) => {
 app.get("/register", (req, res) => {
     console.log(req.query);
     res.status(200).json({ message: "Ok" });
+});
+
+app.get("/setPrice", (req, res) => {
+    let { productId, price } = req.query;
+    productId = productId as string;
+    let data = loadJSON("data.json");
+    data = { ...data };
+
+    if (productId in data) {
+        data[productId].push(price);
+    } else {
+        data[productId] = [price];
+    }
+
+    saveJSON("data.json", data);
+
+    return res.status(200).json({ message: "Working." });
 });
 
 app.listen(port, () => {
