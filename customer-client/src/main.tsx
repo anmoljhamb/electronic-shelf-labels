6f0cef5dcc341e020483cfbd739b386347988d0a
Anmol Jhamb <talktoanmol@outlook.com>
2023-05-05 04:10:36 +0530

    todo added

--- customer-client/src/main.tsx
+++ customer-client/src/main.tsx
@@ -5,6 +5,12 @@ import "./main.scss";
 import { AuthProvider } from "./contexts/AuthContext";
 import { BrowserRouter } from "react-router-dom";
 
+/**
+ * todo create a landing page. redirect to landing page.
+ * todo while sign up, ask for the user's card id.
+ * todo, after signing up, maintain a socket connection with the backend, and just maintain a general cart connection.
+ */
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <>
         <AuthProvider>
