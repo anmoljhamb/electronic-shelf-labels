f3221070b0bf43820e828a91bb36af3e34773a15
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-05 09:40:14 +0530

    Added the code to detect how long a card has been pressed against the RFID for

--- rfid_detection/rfid_detection.ino
+++ rfid_detection/rfid_detection.ino
@@ -11,6 +11,7 @@
 
 #define RST_PIN 4
 #define SS_PIN 21
+#define BAUD_RATE 115200
 
 MFRC522 rfid(SS_PIN, RST_PIN); // Create MFRC522 instance
 void printDec(byte *buffer, byte bufferSize);
@@ -36,7 +37,7 @@ void core1() {
 }
 
 void setup() {
-  Serial.begin(9600); // Initialize serial communications with the PC
+  Serial.begin(BAUD_RATE); // Initialize serial communications with the PC
   while (!Serial)
     ; // Do nothing if no serial port is opened (added for Arduinos based on
       // ATMEGA32U4)
@@ -49,17 +50,33 @@ void setup() {
   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
+unsigned long lastReadTime = 0;
+unsigned long cardStartTime = 0;
+
 void loop() {
-  if (!rfid.PICC_IsNewCardPresent()) {
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+    delay(10);
+    if (cardStartTime == 0)
+      return;
+    unsigned long currTime = millis();
+    unsigned long timeSinceLastRead = currTime - lastReadTime;
+    if (timeSinceLastRead > 50) {
+      // means that we have been here since quite a while since the last read,
+      // and it means that our card has probably been left.
+      unsigned long totalTime = currTime - cardStartTime;
+      Serial.print("Stayed for ");
+      Serial.println(totalTime);
+      cardStartTime = 0;
+    }
     return;
   }
-
-  if (!rfid.PICC_ReadCardSerial()) {
-    return;
+  if (cardStartTime == 0) {
+    cardStartTime = millis();
+    Serial.print("UID: ");
+    printDec(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
   }
-
-  printDec(rfid.uid.uidByte, rfid.uid.size);
-  Serial.println();
+  lastReadTime = millis();
 }
 
 void printDec(byte *buffer, byte bufferSize) {
