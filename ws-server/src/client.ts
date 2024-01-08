34d273eefb8603942322783a2bd726f5f6b4daad
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-08 20:25:04 +0530

    Added multiple websockets on differnt ports

--- ws-server/src/client.ts
+++ ws-server/src/client.ts
@@ -1,6 +1,6 @@
 import ws from 'ws'
 
-const wss = new ws('ws://192.168.1.5:8080')
+const wss = new ws('ws://192.168.1.5:8080/ws2')
 wss.on('open', () => {
   wss.send('hello')
 })
--- ws-server/src/index.ts
+++ ws-server/src/index.ts
@@ -1,12 +1,42 @@
 import { WebSocketServer } from 'ws'
+import http from 'http'
 
-const wss = new WebSocketServer({ port: 8080 })
+const server = http.createServer()
+const wss1 = new WebSocketServer({ noServer: true })
+const wss2 = new WebSocketServer({ noServer: true })
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data)
-    ws.send('i gotcha babe, you esp32')
+wss1.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    const message = msg.toString('utf-8')
+    console.log(message)
   })
+  console.log('Websocket connection on wss1')
+})
 
-  ws.send('something')
+wss2.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    const message = msg.toString('utf-8')
+    console.log(message)
+  })
+  console.log('Websocket connection on wss2')
+})
+
+server.on('upgrade', (request, socket, head) => {
+  const pathname = request.url
+
+  if (pathname === '/ws1') {
+    wss1.handleUpgrade(request, socket, head, (ws) => {
+      wss1.emit('connection', ws, request)
+    })
+  } else if (pathname === '/ws2') {
+    wss2.handleUpgrade(request, socket, head, (ws) => {
+      wss2.emit('connection', ws, request)
+    })
+  } else {
+    socket.destroy()
+  }
+})
+
+server.listen(8080, () => {
+  console.log('listening on port 8080')
 })
