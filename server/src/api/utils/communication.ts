11ed43570db7fe142b7fb1cb3b144c1276ead3a0
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-10 18:54:54 +0530

    Basic communication api added for dealing with web sockets of different clients

---
+++ server/src/api/utils/communication.ts
@@ -0,0 +1,26 @@
+import ws from "ws";
+
+export class Communication {
+  static #sockets: Record<string, ws> = {};
+
+  static hasProduct(productId: string) {
+    return productId in Communication.#sockets;
+  }
+
+  static addSocket(productId: string, socket: ws) {
+    Communication.#sockets[productId] = socket;
+  }
+
+  static getSocket(productId: string) {
+    if (!Communication.hasProduct(productId))
+      throw new Error(`Web Socket not found for ${productId}.`);
+    return Communication.#sockets[productId];
+  }
+
+  static removeSocket(productId: string) {
+    if (!Communication.hasProduct(productId))
+      throw new Error(`Web Socket not found for ${productId}.`);
+    Communication.#sockets[productId].close();
+    delete Communication.#sockets[productId];
+  }
+}
