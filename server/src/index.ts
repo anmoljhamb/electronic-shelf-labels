907fd027a833d1b06875768e3199b21d5d13e26a
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-10 11:43:41 +0530

    basic express app added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+import dotenv from "dotenv";
+import express from "express";
+import path from "path";
+
+dotenv.config({ path: path.join(__dirname, "..", "config.env") });
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Listening on the url *:${PORT}`);
+});
