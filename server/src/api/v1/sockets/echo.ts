df33e607bd3eeebaf91b14cfd6add9b7e543c0af
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-11 07:30:51 +0530

    Send inital price to the esp32

--- server/src/api/v1/sockets/echo.ts
+++ server/src/api/v1/sockets/echo.ts
@@ -1,15 +1,15 @@
 import ws from "ws";
 import { Communication } from "../../utils/communication";
+import { Products } from "../schemas/products";
+import createHttpError from "http-errors";
 
 export const echoWs = new ws.WebSocketServer({ noServer: true });
 
-echoWs.on("connection", (socket, req) => {
+echoWs.on("connection", async (socket, req) => {
   // Todo Add an interval to check for heart beat, as shown in documentation
   const productId = req.headers.product_id as string;
   console.log(`Recvd a socket req on by product ${productId}`);
   Communication.addSocket(productId, socket);
-
-  socket.send("welcome");
 
   socket.on("message", (msg) => {
     console.log("recieved message:", msg.toString());
@@ -27,4 +27,14 @@ echoWs.on("connection", (socket, req) => {
   socket.on("unexpected-response", (resp) => {
     console.log(resp);
   });
+
+  try {
+    const product = await Products.findOne({ productId });
+    if (!product)
+      throw new createHttpError.NotFound("The product was not found.");
+    socket.send(product.price);
+  } catch (err) {
+    console.log(err);
+    socket.send("Product Not Found.");
+  }
 });
